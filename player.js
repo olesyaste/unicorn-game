4b769ac367bfed4c76c88f5dd9911757d8ef555f
timbotimber <hitimstephens@gmail.com>
2020-08-24 17:51:56 +0200

    jumping

--- player.js
+++ player.js
@@ -1,38 +1,39 @@
 class Player {
-    constructor() {
-        this.height = 150;
-        this.width = 150;
-        this.x = 300;
-        this.y = 450;
-        this.image;
-        this.gravity = 0.4;
-        this.velocity = 0;
-    }
+  constructor() {
+    this.height = 150;
+    this.width = 150;
+    this.x = 300;
+    this.y = 450;
+    this.image;
+    this.gravity = 0.8;
+    this.velocity = 0.9;
+  }
 
-    collision(cloudInfo) {
-        let playerX = this.x + this.width / 2;
-        let playerY = this.y + this.height / 2;
-        let cloudX = cloudInfo.x + cloudInfo.width / 2;
-        let cloudY = cloudInfo.y + cloudInfo.height / 2;
-        
-        if (dist(playerX, playerY, cloudX, cloudY) < 50) {
-          return true;
-        }
-      }
-    drawPlayer() {
-        console.log(this.y)
-        this.velocity += this.gravity;
-        this.y += this.velocity;
+  collision(cloudInfo) {
+    console.log(cloudInfo);
+    let playerX = this.x + this.width / 2;
+    let playerY = this.y + this.height / 2;
+    let cloudX = cloudInfo.x + cloudInfo.width / 2;
+    let cloudY = cloudInfo.y + cloudInfo.height / 2;
 
-        if (this.y >= height - this.height) {
-        this.y = (height - this.height);
+    if (dist(playerX, playerY, cloudX, cloudY) < 50) {
+      this.y = cloudInfo.y - 60;
+      return true;
     }
-        image(this.image, this.x, this.y, this.height, this.width);
+  }
+  drawPlayer() {
+    this.velocity += this.gravity;
+    this.y += this.velocity;
+
+    if (this.y >= height - this.height) {
+      this.y = height - this.height;
     }
-    jump() {
-        console.log("this will be the jump");
-        if (this.y === height - this.height) {
-          this.velocity = -10;
-    }
-}
+    image(this.image, this.x, this.y, this.height, this.width);
+  }
+  jump() {
+    console.log("this will be the jump");
+
+    this.velocity = -15;
+    // }
+  }
 }
